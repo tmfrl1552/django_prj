1185bb410cbda8dc346295c3fb3aa004e366e539
tmfrl1552 <tmfrl1552@naver.com>
2020-05-26 15:33:43 +0900

    Second commit

--- django_restapi/api_user/models.py
+++ django_restapi/api_user/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class AppUser(models.Model):
     uid = models.CharField(max_length=20, primary_key=True)
-    age = models.IntegerField()
+    age = models.CharField(max_length=10)
     gender = models.CharField(max_length=10)
     name = models.CharField(max_length=20)
     email = models.CharField(max_length=30)
